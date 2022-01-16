0c3eda333749fee49f146fb54311a7a972f26100
unknown <rodrigocoronel5924@gmail.com>
2022-01-16 01:59:27 -0300

    subiendo presupuesto semanal

---
+++ src/components/Pregunta.js
@@ -0,0 +1,66 @@
+import React, {Fragment, useState} from 'react';
+import PropTypes from 'prop-types';
+import Error from './Error';
+
+const Pregunta = ({guardarPresupuesto, guardarRestante, actualizarPregunta}) => {
+
+    //definir el state
+    const [cantidad, guardarCantidad]= useState(0);
+    const [error, guardarError] = useState(false);
+
+    //Funcion que lee el presupuesto
+    const definirPresupuesto= e => {
+        guardarCantidad(parseInt(e.target.value, 10))
+    }
+
+    //Submit para definir le presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        //Validar
+        if(cantidad < 1 || isNaN ( cantidad )) {
+            guardarError(true);
+            return;
+        }
+
+        //Si se pasa la validacion
+
+        guardarError(false);
+        guardarPresupuesto(cantidad);
+        guardarRestante(cantidad);
+        actualizarPregunta(false);
+    }
+
+
+    return (
+        <Fragment>
+            <h2>Coloca tu presupuesto</h2>
+            {error ? <Error mensaje="El Presupuesto es incorrecto"/> : null}
+
+            <form
+                onSubmit={agregarPresupuesto}
+            >
+                <input
+                    type= "number"
+                    className="u-full-width"
+                    placeholder="coloca tu presupuesto"
+                    onChange={definirPresupuesto}
+                />
+
+                <input
+                    type= "submit"
+                    className="button-primary u-full-width"
+                    value="Definir Presupuesto"
+                />
+            </form>
+        </Fragment>
+    );
+} 
+
+Pregunta.propTypes = {
+    guardarPresupuesto: PropTypes.func.isRequired,
+    guardarRestante: PropTypes.func.isRequired,
+    actualizarPregunta: PropTypes.func.isRequired
+}
+
+export default Pregunta;
